981b574f9ec8046b832f980392b624619d903263
김민철 <kmc901206@gmail.com>
2022-07-27 11:18:47 +0900

    6. 담아둔 목록 > 어댑터 전달 > 어댑터와 화면의 리스트뷰 연결

--- app/src/main/java/com/neppplus/listviewpractice_20220727/MainActivity.kt
+++ app/src/main/java/com/neppplus/listviewpractice_20220727/MainActivity.kt
@@ -2,12 +2,15 @@ package com.neppplus.listviewpractice_20220727
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.listviewpractice_20220727.adapters.StudentListViewAdapter
 import com.neppplus.listviewpractice_20220727.datas.StudentData
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
 //    멤버변수 생성
     val mStudentList = ArrayList<StudentData>()
+    lateinit var mStudentListviewAdapter : StudentListViewAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +23,17 @@ class MainActivity : AppCompatActivity() {
         mStudentList.add( StudentData("윤예은", "인천시 연수구", 1993) )
         mStudentList.add( StudentData("허신지", "서울시 종로구", 1998) )
         mStudentList.add( StudentData("유수아", "서울시 마포구", 1997) )
+        mStudentList.add( StudentData("유수아", "서울시 마포구", 1997) )
+        mStudentList.add( StudentData("유수아", "서울시 마포구", 1991) )
+        mStudentList.add( StudentData("유수아", "서울시 마포구", 1992) )
+        mStudentList.add( StudentData("유수아", "서울시 마포구", 1996) )
+
+//        어댑터 객체화 => 우리가 만들어놓은 ArrayList를 전달
+        mStudentListviewAdapter = StudentListViewAdapter(
+            this, R.layout.student_list_item, mStudentList)
+
+//        xml에 있는 ListView와 어댑터 연결
+        mainListView.adapter = mStudentListviewAdapter
 
     }
 
